c11cdabf1abd310ba0233a20e75e78e19a66f24e
tzhenghao <tanzhao@umich.edu>
2016-02-12 19:08:54 -0500

    added multiple email functionality

--- emailBot.py
+++ emailBot.py
@@ -5,44 +5,85 @@
 # This program is to be used in conjunction with my Task Scheduler Bot that runs
 # as a daemon for handling other tasks, including my emails.
 
+# USAGE: python3 emailBot.py <OUTLOOK, UMICH, GMAIL>
+
 import os, re, logging, sys, pyperclip
+import gmail, yagmail
 
-# Handle my personal Gmail account.
-def gmailHandler(GMAIL):
-	return True
-# Handle my Hotmail.
-def hotmailHandler(HOTMAIL):
-	return True
-# Handle my Umich account.
-def UMichHandler(UMICH):
-	return True
+from myutils import get_myenv
 
-def isLoginGmail(email):
-	return True
+myenv = get_myenv()
 
-def isLoginHotmail(email):
-	return True
+# Load usernames and passwords.
+UMICH_USERNAME = myenv['UMICH_USERNAME']
+UMICH_PASSWORD = myenv['UMICH_PASSWORD']
 
-def isLoginUmich(email):
-	return True
+OUTLOOK_USERNAME = myenv['OUTLOOK_USERNAME']
+OUTLOOK_PASSWORD = myenv['OUTLOOK_PASSWORD']
 
-# EFFECTS: Checks for email type. Returns true on success; false on failure.
-def checkEmail(email):
-	if isLoginGmail(email):
-		return gmailHandler(email)
-	elif isLoginHotmail(email):
-		return hotmailHandler(email)
-	elif isLoginUmich(email):
-		return UMichHandler(email)
-	else:
-		return False
+GMAIL_USERNAME = myenv['GMAIL_USERNAME']
+GMAIL_PASSWORD = myenv['GMAIL_PASSWORD']
 
-# Command line argument handling.
-for email in sys.argv[1:]:
+# EFFECTS: Logs in to the appropriate email provider.`
+def loginEmail(emailAddress):
+	if (emailAddress == GMAIL):
+		loginGmail(emailAddress)
+	elif (emailAddress == OUTLOOK)
+		loginOutlook(emailAddress)
+	elif (emailAddress == HOTMAIL)
+		loginGmail(emailAddress)
 
-	print('Currently handling ' + email + '...')
+# EFFECTS: Sends a reply based on the email address, subject and content provided.
+def sendReply(emailAddress, recipient, subject, content):
+	if (emailAddress == GMAIL):
+		sendReplyGmail(recipient, subject, content)
+	elif (emailAddress == OUTLOOK)
+		sendReplyOutlook(recipient, subject, content)
+	elif (emailAddress == HOTMAIL, subject, content)
+		sendReplyUMich(recipient, subject, content)
 
-	if checkEmail(email) == None:
-		print('There is something wrong with the email provided.')
-		print('Please rerun the program again')
-		sys.exit()
+# EFFECTS: Sends a reply via Gmail.
+def sendReplyGmail(recipient, subject, content):
+	yagmail.register(GMAIL_USERNAME, GMAIL_PASSWORD)
+	yag = yagmail.SMTP(GMAIL_USERNAME)
+	yag.send(
+		to=recipient,
+		subject='RE: {}'.format(subject),
+		contents=content
+	)
+
+
+# EFFECTS: Sends a reply via Outlook.
+def sendReplyOutlook(recipient, subject, contents):
+
+# EFFECTS: Sends a reply via Umich email.
+def sendReplyUMich(recipient, subject, contents):
+	yagmail.register(UMICH_USERNAME, UMICH_PASSWORD)
+	yag = yagmail.SMTP(UMICH_USERNAME)
+	yag.send(
+		to=recipient,
+		subject='RE: {}'.format(subject),
+		contents=content
+	)
+
+# EFFECTS: Returns true if input email macro is valid.
+def checkValidEmail(email):
+	return email == OUTLOOK || email == UMICH || email == GMAIL
+
+def main():
+	# Command line argument handling.
+	for email in sys.argv[1:]:
+
+		if checkValidEmail(email) == None:
+			print('There is something wrong with the email provided.')
+			print('Please rerun the program again')
+			sys.exit()
+
+		print('Currently handling ' + email + '...')
+
+		#TODO
+		loginEmail(email)
+
+
+		print('Done with ' + email)
+
